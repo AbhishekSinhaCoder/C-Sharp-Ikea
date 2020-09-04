04d2ce37a56d08b6a261c80060eb9de397c5207c
Abhishek Sinha <abhisheksinha1506@gmail.com>
2020-09-05 00:01:14 +0100

    Final Commit.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IkeaCataloger {
+    class Program {
+        static void Main (string[] args) {
+            var regex = new Regex ("\"downloadPdfUrl\":\"([^\"]+)\"", RegexOptions.Compiled);
+            Parallel.ForEach (Enumerable.Range (1950, DateTime.Now.Year + 1 - 1950), new ParallelOptions () { MaxDegreeOfParallelism = 8 }, year => {
+                using (var wc = new WebClient ()) {
+                    var pageData = wc.DownloadString ($"https://ikeacatalogues.ikea.com/sv-{year}/page/1");
+                    var matches = regex.Matches (pageData);
+                    if (matches.Count > 0) {
+                        Console.WriteLine ($"Downloading {matches[0].Groups[1].Value} to Ikea -{ year}.pdf");
+                        wc.DownloadFile (matches[0].Groups[1].Value, $"Ikea-{year}.pdf");
+                    }
+                }
+            });
+        }
+    }
+}
